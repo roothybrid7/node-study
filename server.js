64a352ebe63261016efab6cf2c99f1da73b379a9
Satoshi Ohki <roothybrid7@gmail.com>
2011-11-15 09:58:45 +0900

    ejs sample

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var http = require('http');
+var http = require('http')
+  , fs = require('fs')
+  , querystring = require('querystring')
+  , ejs = require('ejs');
+
+function extractParams(url) {
+  return querystring.parse(url.split('?')[1] || '');
+}
 
 var server = http.createServer(function(req, res) {
-  if (req.method == 'POST') {
-    res.writeHead(200, {'Content-Type': req.headers['content-type']});
-    req.on('data', function(data) {
-      res.write(data);
-    });
-    req.on('end', function() {
-      res.end();
-    });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World\n');
-    res.end();
-  }
+  var template = fs.readFileSync(__dirname + '/template.ejs');
+  var params = extractParams(req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write(ejs.render(template.toString(), {locals: {name: params.name}}));
+  res.end();
 }).listen(3000);
