00f29f84bcb1eec011d1543dc44aa3848e41e5f0
Satoshi Ohki <roothybrid7@gmail.com>
2011-11-15 09:48:11 +0900

    POST

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-var http = require('http'), fs = require('fs');
+var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  var read = fs.createReadStream('/tmp/helloworld.html');
-  var head = true;
-  read.on('error', function() {
-    res.writeHead(500, {'Content-Type': 'text/plain'});
-    res.write('IO Error\n');
+  if (req.method == 'POST') {
+    res.writeHead(200, {'Content-Type': req.headers['content-type']});
+    req.on('data', function(data) {
+      res.write(data);
+    });
+    req.on('end', function() {
+      res.end();
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World\n');
     res.end();
-  });
-  read.on('data', function(data) {
-    if (head) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      head = false;
-    }
-    res.write(data);
-  });
-  read.on('end', function(data) {
-    res.end();
-  });
+  }
 }).listen(3000);
